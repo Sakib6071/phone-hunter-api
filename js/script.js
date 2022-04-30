5331ceac80a3b35fe27e108b27dd99dd845db058
Your Name <you@example.com>
2022-05-01 03:24:07 +0600

    implement alert message

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
 function findPhone(){
     const searchTextField=document.getElementById('searchText');
     const searchText=searchTextField.value;
-    console.log(searchText);
+    const url= `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>showData(data.data))
+
+}
+function showData(data){
+    const alertFiled= document.getElementById('alertMessage');
+    alertFiled.textContent="";
+    if(data.length==0){
+        
+        const p=document.createElement('p');
+        p.innerText="Search result not found";
+        p.style.color = "red";
+        p.style.fontSize = "30px";
+        p.style.textAlign="center"
+        p.style.fontWeight="bold"
+        alertFiled.appendChild(p);
+    }else{
+
+
+        for(item of data){
+            console.log(item.phone_name)
+        }
+
+
+
+    }
 
 }
