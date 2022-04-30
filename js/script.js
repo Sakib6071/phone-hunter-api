ed42026ba284efe421d18d8c6c4a5b59aa85f1b5
Your Name <you@example.com>
2022-05-01 03:38:58 +0600

    display mobile and remove previous search

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@ function findPhone(){
 }
 function showData(data){
     const alertFiled= document.getElementById('alertMessage');
+    const phoneContainer=document.getElementById('phone-container');
     alertFiled.textContent="";
+    phoneContainer.textContent="";
     if(data.length==0){
         
         const p=document.createElement('p');
@@ -23,7 +25,20 @@ function showData(data){
 
 
         for(item of data){
-            console.log(item.phone_name)
+            console.log(item);
+            div=document.createElement('div');
+            div.innerHTML=`
+            <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="${item.image}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Name : ${item.phone_name}</h5>
+              <p class="card-text">Brand : ${item.brand}</p>
+              <a href="#" class="btn btn-primary">Explore</a>
+            </div>
+          </div>
+            
+            `
+            phoneContainer.appendChild(div);
         }
 
 
