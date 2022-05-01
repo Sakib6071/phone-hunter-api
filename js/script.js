f07b224eab75afdf63d694ce4056391a20c1ca6e
Your Name <you@example.com>
2022-05-01 18:10:11 +0600

    code cleaning and commenting

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
-function findPhone(){
+/* Find phone function from html onclick */
+const findPhone = () => {
     const searchTextField=document.getElementById('searchText');
     const searchText=searchTextField.value;
+    searchTextField.value="";
     const url= `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     .then(res=>res.json())
     .then(data=>showData(data.data))
-
 }
-function showData(data){
-    console.log(data);
+
+
+/* Show data arrow function get data from findPhone and show results */
+ const showData= (data) =>{
     const alertFiled= document.getElementById('alertMessage');
     const phoneContainer=document.getElementById('phone-container');
     alertFiled.textContent="";
     phoneContainer.textContent="";
     document.getElementById('phoneDetails').textContent="";
+
     if(data.length==0){
-        
         const p=document.createElement('p');
         p.innerText="Search result not found";
         p.style.color = "red";
@@ -23,9 +26,9 @@ function showData(data){
         p.style.textAlign="center"
         p.style.fontWeight="bold"
         alertFiled.appendChild(p);
-    }else{
-
-
+    }
+    else{
+        /* 20 results show at a time */
             for(let i=0;i<20;i++){
                 div=document.createElement('div');
                 div.innerHTML=`
@@ -34,37 +37,30 @@ function showData(data){
                 <div class="card-body">
                   <h5 class="card-title">Name : ${data[i].phone_name}</h5>
                   <p class="card-text">Brand : ${data[i].brand}</p>
-                  <button onclick="explore('${data[i].slug}')" class="btn btn-primary d-block w-75 mx-auto">Explore</button>
+                  <a href="#phoneDetails" onclick="explore('${data[i].slug}')" class="btn btn-primary d-block w-75 mx-auto">Explore</a>
                 </div>
-              </div>
-                
+              </div>               
                 `
-                
                 phoneContainer.appendChild(div);
             }
-/*         for(item of data){
-            //console.log(item.slug);
-
-        }
- */
-
-
     }
-
-
-
-
-
 }
 
-function explore(id){
-       const url = `https://openapi.programming-hero.com/api/phone/${id}`
-       fetch(url)
-       .then(res=>res.json())
-       .then(data=>showSingleDetails(data))
+
+
+/* Explore function and fetch the single item details */
+
+const explore = id =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>showSingleDetails(data))
 }
-function showSingleDetails(data){
-    console.log(data.data)
+
+
+/* Show single item data. */
+const showSingleDetails=data=>{
+    /* console.log(data.data) */
     const singleContainer=document.getElementById('phoneDetails');
     singleContainer.textContent="";
     const div=document.createElement('div');
@@ -78,18 +74,19 @@ function showSingleDetails(data){
     div.innerHTML=`
     <div class="image col-md-6 col-lg-4">
       <img src="${data.data.image}" alt="">
-  </div>
-  <div class="details col-md-6 col-lg-8">
+    </div>
+    <div class="details col-md-6 col-lg-8">
       <h4>Name : ${data.data.name}</h4>
       <h5>Brand : ${data.data.brand}</h5>
       <span>${data.data.releaseDate? data.data.releaseDate : 'Release Date Not Found'}</span></br>
+      <span>Storage : ${data.data.mainFeatures.storage}</span></br>
+      <span>Display Size : ${data.data.mainFeatures.displaySize}</span></br>
       <span>Sensors :${data.data.mainFeatures.sensors[0]} ${data.data.mainFeatures.sensors[1]} ${data.data.mainFeatures.sensors[2]} </span></br>
       <span class="text-primary">Others : </span></br>
       <span>WLAN : ${data?.data?.others?.WLAN? data?.data?.others?.WLAN:'no'}</span></br>
       <span>Bluetooth : ${data?.data?.others?.Bluetooth? data?.data?.others?.Bluetooth:'no'}</span></br>
       <span>GPS : ${data?.data?.others?.GPS? data?.data?.others?.GPS:'no'}</span></br>
-  </div>
-
+    </div>
     `
     singleContainer.appendChild(div);
 }
